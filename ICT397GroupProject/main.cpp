bfebeb2b083b3589a0bcb7a1c12e2824727be565
DarkKnight217 <lochyedward@gmail.com>
2023-04-15 15:39:23 +0800

    Added improved boat model, fixed textures, tidied up rendering code a bit, removed old boat

--- ICT397GroupProject/main.cpp
+++ ICT397GroupProject/main.cpp
@@ -68,6 +68,7 @@ int main()
 
     Shader shader("vertex.shader", "fragment.shader");
     Shader skyboxShader("skyboxVertex.shader", "skyboxFragment.shader");
+    Shader modelShader("modelVertex.shader", "modelFragment.shader");
 
     glm::vec3 camPos = { 0.0f, 250.0f, 0.0f };
     camera.updatePosition(camPos);
@@ -76,7 +77,7 @@ int main()
     landscape.loadFromHeightmap("Terrains/volcanotest.jpg", 8, "Images/Ground2.jpg", GL_TEXTURE_2D);
     water.loadFromFaultFormation(1000, 128, 128, (float)landscape.getTerrain().getWidth()/128, (float)landscape.getTerrain().getHeight()/128, -5, 5, 0.5, "Images/Water1.jpg", GL_TEXTURE_2D);
 
-    Model ourModel("Models/Boat/boat.obj");
+    Model ourModel("Models/NewBoat/boat.obj");
 
     float skyboxVertices[] = {       
         -1.0f,  1.0f, -1.0f,
@@ -158,34 +159,34 @@ int main()
 
         glClearColor(0.0f, 0.5f, 1.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
+     
+        // View and projection matrix setup
         shader.use();
-
         glm::mat4 projection = glm::perspective(glm::radians(90.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 10000.0f);
         shader.setMat4("projection", projection);
-
         glm::mat4 view = glm::lookAt(camera.getCameraPos(), camera.getCameraPos() + camera.getCameraFront(), camera.getCameraUp());
         shader.setMat4("view", view);
 
+        // Landscape render
         glm::mat4 model = glm::mat4(1.0f);
         model = glm::translate(model, { 0.0f, 100.0f, 0.0f });
-        //model = glm::scale(model, {0.5f, 6.0f, 0.5f});
         shader.setMat4("model", model);
-               
         landscape.renderLandscape(camera.getRenderType());
 
-        glm::mat4 model2 = glm::mat4(1.0f);
-        model2 = glm::translate(model2, { 70.0f, 16.5f, 35.0f });
-        shader.setMat4("model", model2);
-
-        ourModel.Draw(shader);
-
-        glm::mat4 model3 = glm::mat4(1.0f);
-        model3 = glm::translate(model3, { 0.0f, 10.0f, 0.0f });
-        shader.setMat4("model", model3);
-        
+        // Water render
+        model = glm::translate(model, { 0.0f, 10.0f, 0.0f });
+        shader.setMat4("model", model);    
         water.renderLandscape(camera.getRenderType());
         
+        // Boat model render
+        modelShader.use();
+        modelShader.setMat4("projection", projection);
+        modelShader.setMat4("view", view);
+        model = glm::translate(model, glm::vec3(0.0f, 1000.0f, 0.0f)); 
+        model = glm::scale(model, glm::vec3(12.0f, 12.0f, 12.0f));	
+        modelShader.setMat4("model", model);
+        ourModel.Draw(modelShader);
+
         /*
         std::vector<glm::vec3> vertices = landscape.getTerrain().getVertices();
 
@@ -201,6 +202,7 @@ int main()
         }
         */
 
+        // Skybox render
         glDepthFunc(GL_LEQUAL);  
         skyboxShader.use();
         view = glm::mat4(glm::mat3(glm::lookAt(camera.getCameraPos(), camera.getCameraPos() + camera.getCameraFront(), camera.getCameraUp())));
