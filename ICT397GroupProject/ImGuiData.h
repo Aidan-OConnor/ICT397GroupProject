a076207e2c0acfeed7c549f44b44b43fb44ead97
Aidan-OConnor <aidantjoc@gmail.com>
2023-05-21 14:52:22 +0800

    Added a "New" button to delete the old map and start making a new one

--- ICT397GroupProject/ImGuiData.h
+++ ICT397GroupProject/ImGuiData.h
@@ -98,120 +98,6 @@ public:
             this->minHeight = this->maxHeight = this->filter = 0;
     }
 
-    /*
-     * @brief Loads in and stores data from Lua
-     *
-     * This function loads in map data from a Lua script
-     * and stores it in a more useable data type
-     *
-     * @param convertedData
-     * @return void
-     */
-    void loadData(std::vector<ImGuiData>& convertedData, std::string fileName)
-    {
-        ImGuiData tempGuiData;
-        luaMap.clear();
-        sol::state lua;
-        lua.open_libraries(sol::lib::base);
-
-        lua.new_usertype<objectData>("objectData", sol::constructors<void()>(),
-            "filepath", &objectData::filepath, "texturePath", &objectData::texturePath, "objectType", &objectData::objectType,
-            "iterations", &objectData::iterations, "width", &objectData::width,
-            "length", &objectData::length, "minHeight", &objectData::minHeight,
-            "maxHeight", &objectData::maxHeight, "filter", &objectData::filter,
-            "tx", &objectData::tx, "ty", &objectData::ty, "tz", &objectData::tz,
-            "sx", &objectData::sx, "sy", &objectData::sy, "sz", &objectData::sz,
-            "rx", &objectData::rx, "ry", &objectData::ry, "rz", &objectData::rz
-        );
-
-        lua.script_file(fileName);
-
-        int numOfTerrain = lua["numTerrains"];
-        int numOfWater = lua["numWater"];
-        int numOfModel = lua["numModels"];
-
-        for (int i = 0; i < numOfTerrain; i++)
-        {
-            tempName = "terrain" + std::to_string(i);
-            const char* terrainName = tempName.c_str();
-            objectData& tempObject = lua[terrainName];
-            luaMap.push_back(tempObject);
-        }
-
-        for (int i = 0; i < numOfWater; i++)
-        {
-            tempName = "water" + std::to_string(i);
-            const char* waterName = tempName.c_str();
-            objectData& tempObject1 = lua[waterName];
-            luaMap.push_back(tempObject1);
-        }
-
-        for (int i = 0; i < numOfModel; i++)
-        {
-            tempName = "model" + std::to_string(i);
-            const char* modelName = tempName.c_str();
-            objectData& tempObject2 = lua[modelName];
-            luaMap.push_back(tempObject2);
-        }
-
-        for (int i = 0; i < luaMap.size(); i++)
-        {
-            std::string objectType = luaMap[i].objectType;
-
-            if (objectType == "Terrain")
-            {
-                Landscape tempTerrain;
-                tempTerrain.loadFromHeightmap(luaMap[i].filepath, 1, luaMap[i].texturePath, GL_TEXTURE_2D);
-                tempTerrain.addTextures("Images/Ground2.jpg", GL_TEXTURE_2D, "Images/Grass.jpg", GL_TEXTURE_2D);
-                tempGuiData.setTerrain(tempTerrain);
-                tempGuiData.setObjectType(luaMap[i].objectType);
-                tempGuiData.setFilePath(luaMap[i].filepath);
-                tempGuiData.setTexturePath(luaMap[i].texturePath);
-            }
-            else if (objectType == "Water")
-            {
-                Landscape tempTerrain;
-                tempTerrain.loadFromFaultFormation(luaMap[i].iterations, luaMap[i].width, luaMap[i].length,
-                    1, 1, luaMap[i].minHeight, luaMap[i].maxHeight, luaMap[i].filter,
-                    luaMap[i].texturePath, GL_TEXTURE_2D);
-                tempGuiData.setTerrain(tempTerrain);
-                tempGuiData.setObjectType(luaMap[i].objectType);
-                tempGuiData.setTexturePath(luaMap[i].texturePath);
-                tempGuiData.setNumIterations(luaMap[i].iterations);
-                tempGuiData.setWidth(luaMap[i].width);
-                tempGuiData.setLength(luaMap[i].length);
-                tempGuiData.setMinHeight(luaMap[i].minHeight);
-                tempGuiData.setMaxHeight(luaMap[i].maxHeight);
-                tempGuiData.setFilter(luaMap[i].filter);
-            }
-            else
-            {
-                tempGuiData.setModel(luaMap[i].filepath);
-                tempGuiData.setObjectType(luaMap[i].objectType);
-                tempGuiData.setFilePath(luaMap[i].filepath);
-            }
-
-            glm::vec3 tempVec;
-
-            tempVec.x = luaMap[i].tx;
-            tempVec.y = luaMap[i].ty;
-            tempVec.z = luaMap[i].tz;
-            tempGuiData.setTranslation(tempVec);
-
-            tempVec.x = luaMap[i].sx;
-            tempVec.y = luaMap[i].sy;
-            tempVec.z = luaMap[i].sz;
-            tempGuiData.setScale(tempVec);
-
-            tempVec.x = luaMap[i].rx;
-            tempVec.y = luaMap[i].ry;
-            tempVec.z = luaMap[i].rz;
-            tempGuiData.setRotation(tempVec);
-
-            convertedData.push_back(tempGuiData);
-        }
-    }
-
     int getNumType(std::string modelType)
     {
         int numOfType = 0;
@@ -615,6 +501,10 @@ public:
                 imGuiObjects.clear();
                 loadData(imGuiObjects, "Test.lua");
             }
+            if (ImGui::Button("New", ImVec2(100, 25)))
+            {
+                imGuiObjects.clear();
+            }
             ImGui::TreePop();
         }
 
@@ -677,6 +567,120 @@ public:
                     imGuiObjects[i].model->Draw(lightingShader);
                 }
             }
+        }
+    }
+
+    /*
+     * @brief Loads in and stores data from Lua
+     *
+     * This function loads in map data from a Lua script
+     * and stores it in a more useable data type
+     *
+     * @param convertedData
+     * @return void
+     */
+    void loadData(std::vector<ImGuiData>& convertedData, std::string fileName)
+    {
+        ImGuiData tempGuiData;
+        luaMap.clear();
+        sol::state lua;
+        lua.open_libraries(sol::lib::base);
+
+        lua.new_usertype<objectData>("objectData", sol::constructors<void()>(),
+            "filepath", &objectData::filepath, "texturePath", &objectData::texturePath, "objectType", &objectData::objectType,
+            "iterations", &objectData::iterations, "width", &objectData::width,
+            "length", &objectData::length, "minHeight", &objectData::minHeight,
+            "maxHeight", &objectData::maxHeight, "filter", &objectData::filter,
+            "tx", &objectData::tx, "ty", &objectData::ty, "tz", &objectData::tz,
+            "sx", &objectData::sx, "sy", &objectData::sy, "sz", &objectData::sz,
+            "rx", &objectData::rx, "ry", &objectData::ry, "rz", &objectData::rz
+        );
+
+        lua.script_file(fileName);
+
+        int numOfTerrain = lua["numTerrains"];
+        int numOfWater = lua["numWater"];
+        int numOfModel = lua["numModels"];
+
+        for (int i = 0; i < numOfTerrain; i++)
+        {
+            tempName = "terrain" + std::to_string(i);
+            const char* terrainName = tempName.c_str();
+            objectData& tempObject = lua[terrainName];
+            luaMap.push_back(tempObject);
+        }
+
+        for (int i = 0; i < numOfWater; i++)
+        {
+            tempName = "water" + std::to_string(i);
+            const char* waterName = tempName.c_str();
+            objectData& tempObject1 = lua[waterName];
+            luaMap.push_back(tempObject1);
+        }
+
+        for (int i = 0; i < numOfModel; i++)
+        {
+            tempName = "model" + std::to_string(i);
+            const char* modelName = tempName.c_str();
+            objectData& tempObject2 = lua[modelName];
+            luaMap.push_back(tempObject2);
+        }
+
+        for (int i = 0; i < luaMap.size(); i++)
+        {
+            std::string objectType = luaMap[i].objectType;
+
+            if (objectType == "Terrain")
+            {
+                Landscape tempTerrain;
+                tempTerrain.loadFromHeightmap(luaMap[i].filepath, 1, luaMap[i].texturePath, GL_TEXTURE_2D);
+                tempTerrain.addTextures("Images/Ground2.jpg", GL_TEXTURE_2D, "Images/Grass.jpg", GL_TEXTURE_2D);
+                tempGuiData.setTerrain(tempTerrain);
+                tempGuiData.setObjectType(luaMap[i].objectType);
+                tempGuiData.setFilePath(luaMap[i].filepath);
+                tempGuiData.setTexturePath(luaMap[i].texturePath);
+            }
+            else if (objectType == "Water")
+            {
+                Landscape tempTerrain;
+                tempTerrain.loadFromFaultFormation(luaMap[i].iterations, luaMap[i].width, luaMap[i].length,
+                    1, 1, luaMap[i].minHeight, luaMap[i].maxHeight, luaMap[i].filter,
+                    luaMap[i].texturePath, GL_TEXTURE_2D);
+                tempGuiData.setTerrain(tempTerrain);
+                tempGuiData.setObjectType(luaMap[i].objectType);
+                tempGuiData.setTexturePath(luaMap[i].texturePath);
+                tempGuiData.setNumIterations(luaMap[i].iterations);
+                tempGuiData.setWidth(luaMap[i].width);
+                tempGuiData.setLength(luaMap[i].length);
+                tempGuiData.setMinHeight(luaMap[i].minHeight);
+                tempGuiData.setMaxHeight(luaMap[i].maxHeight);
+                tempGuiData.setFilter(luaMap[i].filter);
+            }
+            else
+            {
+                tempGuiData.setModel(luaMap[i].filepath);
+                tempGuiData.setObjectType(luaMap[i].objectType);
+                tempGuiData.setFilePath(luaMap[i].filepath);
+            }
+
+            glm::vec3 tempVec;
+
+            tempVec.x = luaMap[i].tx;
+            tempVec.y = luaMap[i].ty;
+            tempVec.z = luaMap[i].tz;
+            tempGuiData.setTranslation(tempVec);
+
+            tempVec.x = luaMap[i].sx;
+            tempVec.y = luaMap[i].sy;
+            tempVec.z = luaMap[i].sz;
+            tempGuiData.setScale(tempVec);
+
+            tempVec.x = luaMap[i].rx;
+            tempVec.y = luaMap[i].ry;
+            tempVec.z = luaMap[i].rz;
+            tempGuiData.setRotation(tempVec);
+
+            convertedData.push_back(tempGuiData);
         }
     }
 
