a84baa049917ad8cf53dc03fb49341ebdf734791
DarkKnight217 <lochyedward@gmail.com>
2023-06-04 19:50:20 +0800

    Fixed some issues with the hut model/textures, should now be loading properly. Added it to the Lua file for models so can be loaded in using the UI now

--- ICT397GroupProject/modelFIles.lua
+++ ICT397GroupProject/modelFIles.lua
@@ -1,4 +1,4 @@
-numModels = 8
+numModels = 9
 
 models = {}
 
@@ -10,3 +10,4 @@ models["Model5"] = "Models/Rocks/rock.obj"
 models["Model6"] = "Models/Rocks/rock2.obj"
 models["Model7"] = "Models/Rocks/rock3.obj"
 models["Model8"] = "Models/Dock/Dock.obj"
+models["Model9"] = "Models/Hut/hut.obj"
