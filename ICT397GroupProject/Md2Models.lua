6a3dcb04623c1e151d4378464b693b16d32df8d6
Aidan-OConnor <aidantjoc@gmail.com>
2023-06-09 12:07:21 +0800

    Added all the models, can only use some since we don't have any weapon attacks and most have a weapon

--- ICT397GroupProject/Md2Models.lua
+++ ICT397GroupProject/Md2Models.lua
@@ -1,7 +1,7 @@
 --For models with no weapon, leave the WeaponF1 and WeaponT1 as ""
 --E.g., md2Models["WeaponF1"] = "", md2Models["WeaponT1"] = ""
 
-numMd2Models = 8
+numMd2Models = 22
 
 md2Models = {}
 
@@ -44,3 +44,73 @@ md2Models["ModelF8"] = "MD2Models/Boss1/Boss1.md2"
 md2Models["ModelT8"] = "MD2Models/Boss1/skin.png"
 md2Models["WeaponF8"] = ""
 md2Models["WeaponT8"] = ""
+
+md2Models["ModelF9"] = "MD2Models/Brain/Brain.md2"
+md2Models["ModelT9"] = "MD2Models/Brain/skin.png"
+md2Models["WeaponF9"] = ""
+md2Models["WeaponT9"] = ""
+
+md2Models["ModelF10"] = "MD2Models/Flipper/Flipper.md2"
+md2Models["ModelT10"] = "MD2Models/Flipper/skin.png"
+md2Models["WeaponF10"] = ""
+md2Models["WeaponT10"] = ""
+
+md2Models["ModelF11"] = "MD2Models/Gladiatr/Gladiatr.md2"
+md2Models["ModelT11"] = "MD2Models/Gladiatr/skin.png"
+md2Models["WeaponF11"] = ""
+md2Models["WeaponT11"] = ""
+
+md2Models["ModelF12"] = "MD2Models/Float/Float.md2"
+md2Models["ModelT12"] = "MD2Models/Float/skin.png"
+md2Models["WeaponF12"] = ""
+md2Models["WeaponT12"] = ""
+
+md2Models["ModelF13"] = "MD2Models/Flyer/Flyer.md2"
+md2Models["ModelT13"] = "MD2Models/Flyer/skin.png"
+md2Models["WeaponF13"] = ""
+md2Models["WeaponT13"] = ""
+
+md2Models["ModelF14"] = "MD2Models/Infantry/Infantry.md2"
+md2Models["ModelT14"] = "MD2Models/Infantry/skin.png"
+md2Models["WeaponF14"] = ""
+md2Models["WeaponT14"] = ""
+
+md2Models["ModelF15"] = "MD2Models/Insane/Insane.md2"
+md2Models["ModelT15"] = "MD2Models/Insane/skin.png"
+md2Models["WeaponF15"] = ""
+md2Models["WeaponT15"] = ""
+
+md2Models["ModelF16"] = "MD2Models/Medic/Medic.md2"
+md2Models["ModelT16"] = "MD2Models/Medic/skin.png"
+md2Models["WeaponF16"] = ""
+md2Models["WeaponT16"] = ""
+
+md2Models["ModelF17"] = "MD2Models/Mutant/Mutant.md2"
+md2Models["ModelT17"] = "MD2Models/Mutant/skin.png"
+md2Models["WeaponF17"] = ""
+md2Models["WeaponT17"] = ""
+
+md2Models["ModelF18"] = "MD2Models/Soldier/Soldier.md2"
+md2Models["ModelT18"] = "MD2Models/Soldier/skin.png"
+md2Models["WeaponF18"] = ""
+md2Models["WeaponT18"] = ""
+
+md2Models["ModelF19"] = "MD2Models/Tank/Tank.md2"
+md2Models["ModelT19"] = "MD2Models/Tank/skin.png"
+md2Models["WeaponF19"] = ""
+md2Models["WeaponT19"] = ""
+
+md2Models["ModelF20"] = "MD2Models/Max/Max.md2"
+md2Models["ModelT20"] = "MD2Models/Max/skin.png"
+md2Models["WeaponF20"] = ""
+md2Models["WeaponT20"] = ""
+
+md2Models["ModelF21"] = "MD2Models/Awolf/Awolf.md2"
+md2Models["ModelT21"] = "MD2Models/Awolf/skin.png"
+md2Models["WeaponF21"] = ""
+md2Models["WeaponT21"] = ""
+
+md2Models["ModelF22"] = "MD2Models/DarkAwolf/DarkAwolf.md2"
+md2Models["ModelT22"] = "MD2Models/DarkAwolf/skin.png"
+md2Models["WeaponF22"] = ""
+md2Models["WeaponT22"] = ""
